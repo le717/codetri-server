468e98cb13d5a06f7552159aa68a1b8cabc599c3
Caleb Ely <le717@users.noreply.github.com>
2022-11-05 14:33:58 -0400

    Properly copy sites-available

--- update-caddy.sh
+++ update-caddy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cp ./Caddyfile /etc/caddy/Caddyfile
-cp -rf ./sites-available/ /etc/caddy/sites-available/
+cp -rf ./sites-available/* /etc/caddy/sites-available/
 systemctl reload caddy
